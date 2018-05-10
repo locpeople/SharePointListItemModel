85d2440773272ae809bf0d7f8bf7cc47b505a96e
unknown <jjongman@locpeople.com>
2018-05-10 13:19:27 +0100

    Updated unit tests

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@ const gulp = require("gulp");
 
 const gulpBabel = require('gulp-babel');
 const ts = require('gulp-typescript');
-const sourcemaps = require("gulp-sourcemaps")
+const sourcemaps = require("gulp-sourcemaps");
+const mocha = require("gulp-mocha");
 
 gulp.task("build", function () {
     var tsproject = ts.createProject("tsconfig.json");
@@ -16,16 +17,21 @@ gulp.task("build", function () {
     tsresult.dts.pipe(gulp.dest("./dist"));
 });
 
-gulp.task("testbuild", function () {
+gulp.task("build-test",["build"], function () {
     var tsproject = ts.createProject("tsconfig.json");
-    var tsresult = gulp.src("./*.ts")
+    var tsresult = gulp.src("./test/**/*.test.ts")
         .pipe(sourcemaps.init())
         .pipe(tsproject());
 
     tsresult.js
         .pipe(gulpBabel({presets: ['es2015']}))
         .pipe(sourcemaps.write())
-        .pipe(gulp.dest("./tmp"))
+        .pipe(gulp.dest("./dist/test"))
 
-    tsresult.dts.pipe(gulp.dest("./tmp"));
+    tsresult.dts.pipe(gulp.dest("./dist/test"));
+});
+
+gulp.task("test", ["build-test"],function() {
+    return gulp.src('./dist/test/**/*.test.js')
+        .pipe(mocha({ui: 'bdd'}))
 });
